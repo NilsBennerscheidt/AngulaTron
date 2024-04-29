1a92b748ef6276f90846522c99a48462c812ad41
nils <nils@bennerscheidt.de>
2024-04-29 23:52:23 +0200

    Daterangepicker Object.entries um das Array zu nutzen

--- src/app/pages/astroids/astroids.component.ts
+++ src/app/pages/astroids/astroids.component.ts
@@ -4,10 +4,23 @@ import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { NasaApiService } from '../../services/nasa-api.service';
 import { NEOFeedResponse, NearEarthObject, NearEarthObjectFormatted } from '../../interfaces/neod';
 
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
+ 
+
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import { start } from 'repl';
+
 @Component({
   selector: 'app-astroids',
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule,MatInputModule, MatFormFieldModule, MatIconModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './astroids.component.html',
   styleUrl: './astroids.component.scss'
 })
@@ -19,11 +32,18 @@ export class AstroidsComponent implements OnInit {
   neodRaw = <NEOFeedResponse>{};
   neodList = signal(<Array<NearEarthObjectFormatted>>[])
   dates = [new Date('2024-02-29'), new Date('2024-02-29')];
-
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
   constructor(private nasaApiService: NasaApiService) { };
 
   ngOnInit(): void {
-    this.dates = [new Date('2024-02-29'), new Date('2024-03-01')];
+    const fromDate = new Date()
+    var untilDate = new Date(fromDate);
+    untilDate.setDate(untilDate.getDate() + 7);
+    this.range.setValue({start: fromDate, end: untilDate})
+    
     this.getAstroids();
   }
 
@@ -31,8 +51,9 @@ export class AstroidsComponent implements OnInit {
    * Get the api Data
    */
   getAstroids() {
-    this.nasaApiService.getAstroids(this.dates[0], this.dates[this.dates.length - 1]).subscribe((data) => {
+    this.nasaApiService.getAstroids(<Date>this.range.getRawValue().start, <Date>this.range.getRawValue().end).subscribe((data) => {
       this.neodRaw = <NEOFeedResponse>data
+      
       this.prepareNeodData(<NEOFeedResponse>data);
     })
   }
@@ -43,20 +64,20 @@ export class AstroidsComponent implements OnInit {
    */
   private prepareNeodData(pNeodData: NEOFeedResponse) {
     let neodList: Array<NearEarthObjectFormatted[]> = [];
-    this.dates.forEach((pElement: Date) => {
-      const dynamicDateKey: string = pElement.toISOString().split('T')[0]
+
+    for (const [key, value] of Object.entries(pNeodData.near_earth_objects)) { 
       try {
         const dailyNeod: any = {
-          date: dynamicDateKey,
-          data: pNeodData.near_earth_objects[dynamicDateKey]
-        } 
-        
+          date: key,
+          data: value
+        }
+
         neodList.push(dailyNeod)
       } catch (error) {
         throw (error);
       }
-    })
-    console.log(neodList)
+    }
+    
     this.neodList.set(<any>neodList)
   }
 }
